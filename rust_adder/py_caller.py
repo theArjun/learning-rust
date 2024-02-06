392572e9ec950ea387dff5d188dae8f474eb0831
thearjun <mailarjunadhikari@gmail.com>
2024-02-06 22:43:22 +0545

    Update files

--- rust_adder/py_caller.py
+++ rust_adder/py_caller.py
@@ -1,37 +1,14 @@
 from ctypes import cdll, c_int
-import time
 
+# Load the shared library. Adjust the path as necessary.
+lib = cdll.LoadLibrary("./target/release/librust_adder.dylib")
 
-# Load the library (use the appropriate file name for your OS)
-lib = cdll.LoadLibrary('./target/release/librust_adder.dylib')  # or .dylib, .dll
+add_numbers = lib.add_numbers
 
 # Set the argument and return types
-lib.add_numbers.argtypes = (c_int, c_int)
-lib.add_numbers.restype = c_int
+add_numbers.argtypes = (c_int, c_int)
+add_numbers.restype = c_int
 
-# Call the function
-start_time = time.perf_counter()
+# Call the Rust function
 result = lib.add_numbers(5, 7)
-end_time = time.perf_counter()
-
-rust_time = end_time - start_time
-
-print("From rust: ", end_time - start_time)
-
-
-def add_numbers(a, b):
-    return a + b
-
-
-# In python
-start_time = time.perf_counter()
-for i in range(100_000_000):
-    result = add_numbers(5, 7)
-end_time = time.perf_counter()
-python_time = end_time - start_time
-
-rust_is_fast_by = python_time / rust_time * 100
-print(f"Rust is fast by : {rust_is_fast_by:2f}")
-
-
-
+print(f"The result is: {result}")
--- rust_adder/src/lib.rs
+++ rust_adder/src/lib.rs
@@ -1,18 +1,4 @@
-// This attribute tells Rust to use C naming conventions for the add_numbers function.
 #[no_mangle]
 pub extern "C" fn add_numbers(a: i32, b: i32) -> i32 {
-
-    let mut count = 100_000_000;
-    let mut result: i32;
-
-    loop {
-        result = a + b;
-        count -=1;
-        if count == 0 {
-            break;
-        }
-    }
-
-    result
+    a + b
 }
-
