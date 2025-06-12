b2f9a2832d51e0a179a1577bc2863a31fd832bab
thearjun <33110520+theArjun@users.noreply.github.com>
2025-06-12 21:42:08 +0545

    feat: add WebAssembly calculator with basic arithmetic functions and README

---
+++ exercises/webassembly/calculator/pkg/calculator.d.ts
@@ -0,0 +1,41 @@
+/* tslint:disable */
+/* eslint-disable */
+export function add(a: number, b: number): number;
+export function subtract(a: number, b: number): number;
+export function multiply(a: number, b: number): number;
+export function divide(a: number, b: number): number | undefined;
+export function modulo(a: number, b: number): number | undefined;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly add: (a: number, b: number) => number;
+  readonly subtract: (a: number, b: number) => number;
+  readonly multiply: (a: number, b: number) => number;
+  readonly divide: (a: number, b: number) => number;
+  readonly modulo: (a: number, b: number) => number;
+  readonly __wbindgen_export_0: WebAssembly.Table;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ exercises/webassembly/calculator/pkg/calculator_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const add: (a: number, b: number) => number;
+export const subtract: (a: number, b: number) => number;
+export const multiply: (a: number, b: number) => number;
+export const divide: (a: number, b: number) => number;
+export const modulo: (a: number, b: number) => number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_start: () => void;
