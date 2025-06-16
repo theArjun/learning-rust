e3e9240ffd4cb7bc6681a583ed70b148aa7c71ff
thearjun <33110520+theArjun@users.noreply.github.com>
2025-06-16 22:34:28 +0545

    feat: add initial implementation for number to words conversion with title casing

---
+++ exercises/numbers/p001-num2words/src/main.rs
@@ -0,0 +1,15 @@
+use num2words::Num2Words;
+use titlecase::{Titlecase};
+
+fn main() {
+    let number = 1456;
+    let words = Num2Words::new(number).to_words();
+    match words {
+        Ok(value) => {
+            println!("{:?}", value.titlecase());
+        }
+        Err(e) => {
+            dbg!(e);
+        }
+    }
+}
